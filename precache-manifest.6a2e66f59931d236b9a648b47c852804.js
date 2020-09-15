0785a3a8ff20c5ccef96b97bf82c5752b63a82eb
Apoerin <apoerin73@gmail.com>
2020-09-15 10:21:30 +0200

    Updates

---
+++ precache-manifest.6a2e66f59931d236b9a648b47c852804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e5aea1ca33df79820d70c35750fa5b",
+    "url": "/images/index.html"
+  },
+  {
+    "revision": "6c11c83b64844c2b9892",
+    "url": "/images/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63997784223398b35983",
+    "url": "/images/static/js/2.dd9fcf57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/images/static/js/2.dd9fcf57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c11c83b64844c2b9892",
+    "url": "/images/static/js/main.a18ab06a.chunk.js"
+  },
+  {
+    "revision": "55f9d55762268070c5a2",
+    "url": "/images/static/js/runtime-main.01570eda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/images/static/media/logo.5d5d9eef.svg"
+  }
+]);
